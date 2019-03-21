81575fa6f23ac44698e4e108fbdf2ae93a9a5dfb
mrdr3ad <byvuegfx@gmail.com>
2019-03-21 01:15:56 +0000

    Data storage
    began to implement the local database storage for the schedules, isnt yet complete but the foundations have been laid

---
+++ ScheduleV5/Repositories/ScheduleHelper.cs
@@ -0,0 +1,40 @@
+﻿using Android.Content;
+using Android.Database.Sqlite;
+
+namespace ScheduleV5
+{
+    public class ScheduleHelper
+    {
+        private const string tableName = "ScheduleTable";
+        private const string columnEventName = "EventName";
+        private const string columnOrganiser = "Organiser";
+        private const string columnID = "ContactID";
+        private const string columnCapacity = "Capacity";
+        private const string columnFrom = "From";
+        private const string columnTo = "To";
+        private const string columnAllDay = "AllDay";
+        private const string columnColor = "Color";
+
+        public const string createQuery = "CREATE TABLE " + tableName + "(" + columnEventName + " INTEGER PRIMARY KEY," + columnOrganiser + " TEXT," + columnID + " TEXT" + columnCapacity + " TEXT" + columnFrom + " TEXT" + columnTo + " TEXT)";
+        public const string deleteQuery = "DROP TABLE IF EXISTS" + tableName;
+
+        public ScheduleHelper()
+        {
+        }
+
+        public static void InsertScheduleData(Context context, Appointments appointments)
+        {
+            SQLiteDatabase db = new DataStore(context).WritableDatabase;
+            ContentValues contentValues = new ContentValues();
+                contentValues.Put(columnEventName, appointments.EventName);
+                contentValues.Put(columnOrganiser, appointments.Organizer);
+                contentValues.Put(columnID, appointments.ContactID);
+                contentValues.Put(columnCapacity, appointments.Capacity);
+                contentValues.Put(columnFrom, appointments.From.ToString()); //temporary fix 
+                contentValues.Put(columnTo, appointments.To.ToString());//temp fix 
+                contentValues.Put(columnAllDay, appointments.AllDay);
+            db.Insert(tableName, null, contentValues);
+            db.Close();   
+        }
+    }
+}
