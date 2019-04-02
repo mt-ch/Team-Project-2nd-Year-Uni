7d527851f75887cb5518511a203887f24abb0310
matt <100412015@unimail.derby.ac.uk>
2019-04-02 23:05:05 +0100

    started adding sqlite

---
+++ AppointmentInfo.cs
@@ -0,0 +1,79 @@
+﻿using Syncfusion.XForms.DataForm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.XForms.Buttons;
+
+
+namespace ScheduleV5
+{
+    public class AppointmentInfo
+    {
+        private string appointmentName;
+        private DateTime? startDate;
+        private DateTime? endDate;
+        private DateTime? startTime;
+        private DateTime? endTime;
+
+        public AppointmentInfo()
+        {
+
+        }
+
+        [Display(Name = "Event Name:")]
+        public string AppointmentName
+        {
+            get { return this.appointmentName; }
+            set
+            {
+                this.appointmentName = value;
+            }
+        }
+
+        [Display(Name = "Start Date:")]
+        public DateTime? StartDate
+        {
+            get { return startDate; }
+            set
+            {
+                startDate = value;
+            }
+        }
+
+        [DataType(DataType.Time)]
+        [Display(Name = "Start Time:")]
+        public DateTime? StartTime
+        {
+            get { return startTime; }
+            set
+            {
+                startTime = value;
+            }
+        }
+
+        [Display(Name = "End Date:")]
+        public DateTime? EndDate
+        {
+            get { return endDate; }
+            set
+            {
+                endDate = value;
+            }
+        }
+
+        [DataType(DataType.Time)]
+        [Display(Name = "End Time:")]
+        public DateTime? EndTime
+        {
+            get { return endTime; }
+            set
+            {
+                endTime = value;
+            }
+        }
+
+
+    }
+}
---
+++ AppointmentsDataForm.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Syncfusion.XForms.DataForm;
+using Syncfusion.XForms.Buttons;
+
+
+using Xamarin.Forms;
+
+namespace ScheduleV5
+{
+    public partial class AppointmentsDataForm : ContentPage
+    {
+
+
+        public AppointmentsDataForm()
+        {
+            InitializeComponent();
+
+
+            dataForm.AutoGeneratingDataFormItem += DataForm_AutoGeneratingDataFormItem;
+
+
+        }
+
+        private void DataForm_AutoGeneratingDataFormItem(object sender, AutoGeneratingDataFormItemEventArgs e)
+        {
+            if (e.DataFormItem != null && e.DataFormItem.Name == "StartDate")
+            {
+                (e.DataFormItem as DataFormDateItem).Format = "ddd, MMM d, yyyy";
+                (e.DataFormItem as DataFormDateItem).MinimumDate = DateTime.Now;
+            }
+            
+            if (e.DataFormItem != null && e.DataFormItem.Name == "EndDate")
+            {
+                (e.DataFormItem as DataFormDateItem).Format = "ddd, MMM d, yyyy";
+                (e.DataFormItem as DataFormDateItem).MinimumDate = DateTime.Now;
+            }
+
+            if (e.DataFormItem != null && e.DataFormItem.Name == "StartTime")
+            {
+                (e.DataFormItem as DataFormTimeItem).Format = "HH:mm";
+            }
+
+            if (e.DataFormItem != null && e.DataFormItem.Name == "EndTime")
+            {
+                (e.DataFormItem as DataFormTimeItem).Format = "HH:mm";
+            }
+
+        }
+    }
+}
